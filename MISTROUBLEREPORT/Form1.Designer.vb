bbbb683ba91b1e6b7a1741eea6b873c79aa5ef7c
MIS - Rafael <MIS - Rafael@MIS-PETER>
2024-06-10 15:46:12 +0800

    >HELLO

---
+++ MISTROUBLEREPORT/Form1.Designer.vb
@@ -0,0 +1,354 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btn_CreateTicket = New System.Windows.Forms.Button()
+        Me.btn_Find = New System.Windows.Forms.Button()
+        Me.btn_Settings = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.btn_Submit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_Description = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_ticketnumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_Name = New System.Windows.Forms.TextBox()
+        Me.combo_Department = New System.Windows.Forms.ComboBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.btn_Print = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column8, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7})
+        Me.DataGridView1.Location = New System.Drawing.Point(149, 119)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(452, 266)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "id"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "start_date"
+        Me.Column8.Name = "Column8"
+        Me.Column8.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "name"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "ticket_number"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "description"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "level"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "completed_date"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = "risk_taking"
+        Me.Column7.Name = "Column7"
+        Me.Column7.ReadOnly = True
+        '
+        'btn_CreateTicket
+        '
+        Me.btn_CreateTicket.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_CreateTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_CreateTicket.Location = New System.Drawing.Point(12, 106)
+        Me.btn_CreateTicket.Name = "btn_CreateTicket"
+        Me.btn_CreateTicket.Size = New System.Drawing.Size(100, 40)
+        Me.btn_CreateTicket.TabIndex = 1
+        Me.btn_CreateTicket.Text = "Create Ticket"
+        Me.btn_CreateTicket.UseVisualStyleBackColor = True
+        '
+        'btn_Find
+        '
+        Me.btn_Find.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_Find.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Find.Location = New System.Drawing.Point(12, 152)
+        Me.btn_Find.Name = "btn_Find"
+        Me.btn_Find.Size = New System.Drawing.Size(100, 40)
+        Me.btn_Find.TabIndex = 2
+        Me.btn_Find.Text = "Find"
+        Me.btn_Find.UseVisualStyleBackColor = True
+        '
+        'btn_Settings
+        '
+        Me.btn_Settings.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_Settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Settings.Location = New System.Drawing.Point(12, 244)
+        Me.btn_Settings.Name = "btn_Settings"
+        Me.btn_Settings.Size = New System.Drawing.Size(100, 40)
+        Me.btn_Settings.TabIndex = 3
+        Me.btn_Settings.Text = "Settings"
+        Me.btn_Settings.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(118, 2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(679, 450)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.btn_Submit)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.txt_Description)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.txt_ticketnumber)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.txt_Name)
+        Me.TabPage1.Controls.Add(Me.combo_Department)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(671, 424)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'btn_Submit
+        '
+        Me.btn_Submit.BackColor = System.Drawing.Color.LimeGreen
+        Me.btn_Submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Submit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Submit.ForeColor = System.Drawing.Color.White
+        Me.btn_Submit.Location = New System.Drawing.Point(300, 331)
+        Me.btn_Submit.Name = "btn_Submit"
+        Me.btn_Submit.Size = New System.Drawing.Size(89, 36)
+        Me.btn_Submit.TabIndex = 7
+        Me.btn_Submit.Text = "Submit"
+        Me.btn_Submit.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(97, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(138, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Describe the Problem"
+        '
+        'txt_Description
+        '
+        Me.txt_Description.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Description.ForeColor = System.Drawing.SystemColors.InfoText
+        Me.txt_Description.HideSelection = False
+        Me.txt_Description.Location = New System.Drawing.Point(100, 220)
+        Me.txt_Description.Name = "txt_Description"
+        Me.txt_Description.Size = New System.Drawing.Size(484, 96)
+        Me.txt_Description.TabIndex = 5
+        Me.txt_Description.Text = "Type Here."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(157, 133)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Ticket Number"
+        '
+        'txt_ticketnumber
+        '
+        Me.txt_ticketnumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_ticketnumber.Location = New System.Drawing.Point(160, 152)
+        Me.txt_ticketnumber.Name = "txt_ticketnumber"
+        Me.txt_ticketnumber.Size = New System.Drawing.Size(279, 31)
+        Me.txt_ticketnumber.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(97, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Name"
+        '
+        'txt_Name
+        '
+        Me.txt_Name.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Name.Location = New System.Drawing.Point(100, 84)
+        Me.txt_Name.Name = "txt_Name"
+        Me.txt_Name.Size = New System.Drawing.Size(162, 31)
+        Me.txt_Name.TabIndex = 1
+        '
+        'combo_Department
+        '
+        Me.combo_Department.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.combo_Department.FormattingEnabled = True
+        Me.combo_Department.Items.AddRange(New Object() {"HR", "MKK", "CSMC", "LOGISTIC"})
+        Me.combo_Department.Location = New System.Drawing.Point(317, 82)
+        Me.combo_Department.Name = "combo_Department"
+        Me.combo_Department.Size = New System.Drawing.Size(267, 32)
+        Me.combo_Department.TabIndex = 0
+        Me.combo_Department.Text = "Select Department"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DataGridView1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(671, 424)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(671, 424)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "TabPage3"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(671, 424)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "TabPage4"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'btn_Print
+        '
+        Me.btn_Print.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn_Print.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_Print.Location = New System.Drawing.Point(12, 198)
+        Me.btn_Print.Name = "btn_Print"
+        Me.btn_Print.Size = New System.Drawing.Size(100, 40)
+        Me.btn_Print.TabIndex = 5
+        Me.btn_Print.Text = "Print"
+        Me.btn_Print.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_Print)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.btn_Settings)
+        Me.Controls.Add(Me.btn_Find)
+        Me.Controls.Add(Me.btn_CreateTicket)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MIS TROUBLE REPORT"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents btn_CreateTicket As Button
+    Friend WithEvents btn_Find As Button
+    Friend WithEvents btn_Settings As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents btn_Print As Button
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents combo_Department As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_Name As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_Description As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_ticketnumber As TextBox
+    Friend WithEvents btn_Submit As Button
+End Class
---
+++ MISTROUBLEREPORT/Form1.vb
@@ -0,0 +1,133 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+Public Class Form1
+    Dim connectionString As String = "server=194.110.173.106;port=3306;database=bbox_logistics;user=bbox_logistics;password=1234;"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TabControl1.Anchor = AnchorStyles.Top Or AnchorStyles.Left
+        MaximizeBox = False
+    End Sub
+
+    Private Sub btn_CreateTicket_Click(sender As Object, e As EventArgs) Handles btn_CreateTicket.Click
+        TabControl1.SelectedIndex = 0
+    End Sub
+
+    Private Sub btn_Find_Click(sender As Object, e As EventArgs) Handles btn_Find.Click
+        TabControl1.SelectedIndex = 1
+    End Sub
+
+    Private Sub btn_Print_Click(sender As Object, e As EventArgs) Handles btn_Print.Click
+        TabControl1.SelectedIndex = 2
+    End Sub
+
+    Private Sub btn_Settings_Click(sender As Object, e As EventArgs) Handles btn_Settings.Click
+        TabControl1.SelectedIndex = 3
+    End Sub
+
+#Region "Tab1"
+
+    Dim ticketCounter As Integer = 0
+
+    Private Sub combo_Department_SelectedIndexChanged(sender As Object, e As EventArgs) Handles combo_Department.SelectedIndexChanged
+        Dim currentDate As String = DateTime.Now.ToString("ddMMyyyy")
+        Dim departmentInitials As String = combo_Department.SelectedItem.ToString().Substring(0, 2)
+        Dim ticketNumber As String = currentDate & departmentInitials & GetNextTicketNumber().ToString()
+        txt_ticketnumber.Text = ticketNumber
+    End Sub
+
+    Private Function GetNextTicketNumber() As Integer
+        Dim maxTicketNumber As Integer = 0
+
+        Using connection As New MySqlConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "SELECT MAX(CAST(SUBSTRING_INDEX(ticket_number, '" & DateTime.Now.ToString("ddMMyyyy") & combo_Department.SelectedItem.ToString().Substring(0, 2) & "', -1) AS UNSIGNED)) AS max_ticket_number FROM tb_MIS WHERE ticket_number LIKE '" & DateTime.Now.ToString("ddMMyyyy") & combo_Department.SelectedItem.ToString().Substring(0, 2) & "%'"
+
+            Using command As New MySqlCommand(query, connection)
+                Dim reader As MySqlDataReader = command.ExecuteReader()
+
+                If reader.Read() AndAlso Not reader.IsDBNull(0) Then
+                    maxTicketNumber = Convert.ToInt32(reader("max_ticket_number"))
+                End If
+
+                reader.Close()
+            End Using
+
+            connection.Close()
+        End Using
+
+        ticketCounter = maxTicketNumber + 1
+        Return ticketCounter
+    End Function
+
+    Private Sub txt_Name_TextChanged(sender As Object, e As EventArgs) Handles txt_Name.TextChanged
+
+    End Sub
+
+    Private Sub txt_Description_TextChanged(sender As Object, e As EventArgs) Handles txt_Description.TextChanged
+
+    End Sub
+
+    Private Sub txt_ticketnumber_TextChanged(sender As Object, e As EventArgs) Handles txt_ticketnumber.TextChanged
+
+    End Sub
+
+    Private Sub btn_Submit_Click(sender As Object, e As EventArgs) Handles btn_Submit.Click
+        Dim startDate As DateTime = DateTime.Now
+        Dim name As String = txt_Name.Text
+        Dim ticketNumber As String = txt_ticketnumber.Text
+        Dim description As String = txt_Description.Text
+        Dim level As String = "Low Impact"
+        Dim completedDate As DateTime? = Nothing
+        Dim riskRating As String = Nothing
+
+        Using connection As New MySqlConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "INSERT INTO tb_MIS (start_date, name, ticket_number, description, level, completed_date, risk_rating) VALUES (@start_date, @name, @ticket_number, @description, @level, @completed_date, @risk_rating)"
+
+            Using command As New MySqlCommand(query, connection)
+                command.Parameters.AddWithValue("@start_date", startDate)
+                command.Parameters.AddWithValue("@name", name)
+                command.Parameters.AddWithValue("@ticket_number", ticketNumber)
+                command.Parameters.AddWithValue("@description", description)
+                command.Parameters.AddWithValue("@level", level)
+                command.Parameters.AddWithValue("@completed_date", If(completedDate.HasValue, completedDate.Value, DBNull.Value))
+                command.Parameters.AddWithValue("@risk_rating", If(String.IsNullOrEmpty(riskRating), DBNull.Value, riskRating))
+
+                command.ExecuteNonQuery()
+            End Using
+
+            connection.Close()
+        End Using
+
+        ' Clear the input fields after submitting
+        txt_Name.Text = ""
+        txt_ticketnumber.Text = ""
+        txt_Description.Text = ""
+
+        ' Display a message or perform any additional actions after submitting
+        MessageBox.Show("Ticket submitted successfully!")
+    End Sub
+
+
+#End Region
+
+#Region "Tab2"
+
+#End Region
+
+#Region "Tab3"
+
+#End Region
+
+#Region "Tab4"
+
+#End Region
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+End Class
